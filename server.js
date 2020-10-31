833163e251874a6781ca9ec6015c1998a34299eb
teaven14 <70546459+teaven14@users.noreply.github.com>
2020-10-30 20:28:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js'
+import fetch from 'node-fetch';
+
 dotenv.config();
 
 
@@ -21,16 +22,19 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async(req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    console.log('fetch request data', data);
+   
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-    res.json(countries);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
